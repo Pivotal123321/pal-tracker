5ed3328b5dcf745a57c4b80f93e69296ac9136c2
Ubuntu <ubuntu@ip-172-31-15-187.ap-southeast-1.compute.internal>
2021-07-01 05:33:21 +0000

    Binding Spring JDC template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,15 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
+
+    //DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
-    @Bean
-    public TimeEntryRepository getTimeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
-        }
+//    @Bean
+//    public TimeEntryRepository getTimeEntryRepository() {
+//        return new InMemoryTimeEntryRepository();
+//        }
+
+//    @Bean
+//    public TimeEntryRepository getJdbcTimeEntryRepository() {
+//        return new JdbcTimeEntryRepository(dataSource);
+//    }
+
 }
